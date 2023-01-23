5c9bb8bca0a96a0f3babe522fc445204ccf1337d
Md. Rezuan Hussen <rzuanhussen.rh@gmail.com>
2023-01-23 21:09:54 +0600

    Calculate Total value

--- openXl.py
+++ openXl.py
@@ -6,17 +6,34 @@ file = openpyxl.load_workbook('inventory.xlsx')
 product_details = file['Sheet1']
 
 products_company = {}
+products_cost_per_company = {}
 
 for product_row in range(2, product_details.max_row + 1):
+    # Total Company
     company_name = product_details.cell(product_row, 4).value
     if company_name in products_company:
         products_company[company_name] = products_company.get(company_name) + 1
     else:
         products_company[company_name] = 1
 
+    # Total Cost per Company
+    product_quantity = product_details.cell(product_row, 2).value
+    product_price = product_details.cell(product_row, 3).value
+
+    if company_name in products_cost_per_company:
+        products_cost_per_company[company_name] = products_cost_per_company.get(company_name) +\
+                                                  (product_quantity * product_price)
+    else:
+        products_cost_per_company[company_name] = product_quantity * product_price
+
+
+
+
 
 
 print(products_company)
 
+print(products_cost_per_company)
+print(sum(products_cost_per_company.values()))
 
 
